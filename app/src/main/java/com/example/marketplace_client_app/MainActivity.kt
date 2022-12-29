7a7dd896f5025b58aaf3e611a21c3a32a6deb833
Youssef434 <fatmi2019py@gmail.com>
2022-12-29 22:08:28 +0100

    added the dummy list of products.

--- app/src/main/java/com/example/marketplace_client_app/MainActivity.kt
+++ app/src/main/java/com/example/marketplace_client_app/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.marketplace_client_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marketplace_client_app.model.Categorie
+import com.example.marketplace_client_app.model.Produit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val products: List<Produit> = initProducts()
+    }
+
+    private fun initProducts(): List<Produit> {
+        return listOf(
+            Produit(
+                R.drawable.book,
+                1,
+                "Book1",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                50,
+                listOf(Categorie(1, "book"))
+            ),
+            Produit(
+                R.drawable.book,
+                2,
+                "Book2",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                150,
+                listOf(Categorie(1, "book"))
+            ),
+            Produit(
+                R.drawable.book,
+                3,
+                "Book3",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                80,
+                listOf(Categorie(1, "book"))
+            )
+        )
     }
 }
--- app/src/main/java/com/example/marketplace_client_app/model/Produit.kt
+++ app/src/main/java/com/example/marketplace_client_app/model/Produit.kt
@@ -1,10 +1,14 @@
 package com.example.marketplace_client_app.model
 
 data class Produit(
-    val numeroSerie: String,
+    val image: Int,
+    val numeroSerie: Int,
     val nom: String,
     val description: String,
     val prix: Int,
-    val disponible: Boolean,
     val categories: List<Categorie>
-)
+) {
+    fun estDisponible(): Boolean {
+        return true
+    }
+}
